859062e93f2a8affd3382905324e91f5be38a5d2
leekwanghyup <leekwanghyup@naver.com>
2022-05-25 14:01:46 +0900

    로그인

--- src/main/java/me/light/controller/HomeController.java
+++ src/main/java/me/light/controller/HomeController.java
@@ -1,15 +1,18 @@
 package me.light.controller;
 
-import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 	
-	
 	@GetMapping("/home")
 	public String home() {
 		return "welcome";
+	}
+	
+	@GetMapping("/login")
+	public String login() {
+		return "login";
 	}
 }
